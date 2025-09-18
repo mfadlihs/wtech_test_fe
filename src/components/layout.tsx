b5dd2206d5e348fa0d00d815987aca78f3eee1f5
mfadlihs <winnerfadli999@gmail.com>
2025-09-18 13:57:46 +0700

    add link placeholder to get back home

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50">
@@ -7,9 +9,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <nav className="flex items-center justify-between h-16">
             <div className="flex items-center space-x-8">
               <div className="flex-shrink-0">
-                <h1 className="text-2xl font-bold text-gray-900">
-                  Picture Gallery
-                </h1>
+                <Link href="/">
+                  <h1 className="text-2xl font-bold text-gray-900">
+                    Picture Gallery
+                  </h1>
+                </Link>
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-8">
